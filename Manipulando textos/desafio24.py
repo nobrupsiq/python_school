30e7ee0cb410e8ccc77cbf054bec9a15ce444af8
Bruno <bpires.siqueira@gmail.com>
2024-08-30 17:08:57 -0300

    feat: desafio24

--- Manipulando textos/desafio24.py
+++ Manipulando textos/desafio24.py
@@ -1,1 +1,4 @@
 # Crie um programa que leia no nome de uma cidade e diga se ela começa ou nao com o nome "SANTO".
+
+city_name = input('Em que cidade voce nasceu? ').strip()
+print(city_name[:5].upper() == 'SANTO')
