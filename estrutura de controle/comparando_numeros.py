7ec813bba645b7ef1092daf05dcdd8549fb2b7a9
Bruno <bpires.siqueira@gmail.com>
2024-09-25 15:43:01 -0300

    feat: desafio38 comparando numeros

---
+++ estrutura de controle/comparando_numeros.py
@@ -0,0 +1,9 @@
+n1 = float(input('Primeiro numero: '))
+n2 = float(input('Segundo numero: '))
+
+if n1 > n2:
+    print('O PRIMEIRO valor é maior')
+elif n1 == n2:
+    print('VALORES iguais')
+else:
+    print('O SEGUNDO valor é maior')
