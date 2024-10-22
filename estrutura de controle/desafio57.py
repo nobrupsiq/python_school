3438d3aa82cdf9418ad40140273fbb9d0d2433ed
Bruno <bpires.siqueira@gmail.com>
2024-10-22 17:28:43 -0300

    feat: desafio57 da introducao

---
+++ estrutura de controle/desafio57.py
@@ -0,0 +1,12 @@
+"""
+Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'.
+Caso esteja errado, peça a digitação novamente até ter um valor correto.
+"""
+
+sexo = ''
+
+while sexo != 'M' and sexo != 'F':
+  sexo_do_usuario = input('Digite o seu Sexo "M" ou "F": ')
+  sexo = sexo_do_usuario
+  if sexo != 'M' and sexo != 'F':
+    print(f'Valor incorreto, você digitou {sexo}. Digite "M" ou "F"')
