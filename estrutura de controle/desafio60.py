6c7cbb938c6181e2495ed81525e93ef057e2ca03
bruno <bpires.siqueira@gmail.com>
2024-10-29 11:47:35 -0300

    3 modos de fatorial

--- estrutura de controle/desafio60.py
+++ estrutura de controle/desafio60.py
@@ -3,22 +3,33 @@ Faça um programa que leia um número qualquer e mostre o seu fatorial.
 """
 
 #UTILIZANDO MÓDULO
-# from math import factorial
+from math import factorial
 
-# numero = int(input('Digite um número para calcular seu fatorial: '))
-# fatorial = factorial(numero)
-# print(f'O fatorial de {numero} é {fatorial}')
+numero = int(input('Digite um número para calcular seu fatorial: '))
+fatorial = factorial(numero)
+print(f'O fatorial de {numero} é {fatorial}')
 
 # MÉTODO TRADICIONAL
 
-numero = int(input('Digite um número para calcular seu fatorial: '))
-pointer = numero
-acumulador = 1
+numero2 = int(input('Digite um número para calcular seu fatorial: '))
+pointer = numero2
+acumulador2 = 1
 
-print(f'Calculando {numero}! = ', end='')
+print(f'Calculando {numero2}! = ', end='')
 while pointer > 0:
   print(f'{pointer}', end="")
   print(' x ' if pointer > 1 else ' = ', end="")
-  acumulador *= pointer
+  acumulador2 *= pointer
   pointer -= 1
-print(acumulador)
+print(acumulador2)
+
+
+# DESAFIO: FAZER TAMBÉM COM O LAÇO 'FOR'
+numero3 = int(input('Digite um número para calcular seu fatorial: '))
+acumulador3 = 1
+
+for i in range(numero3, -1+1, -1):
+  print(f'{i}', end='')
+  print(' x ' if i > 1 else ' = ', end='')
+  acumulador3 *= i
+print(f'{acumulador3}')
