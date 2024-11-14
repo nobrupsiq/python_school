5e87f0530ace3e851a8e3c72422f2f87b6fbead2
bruno <bpires.siqueira@gmail.com>
2024-11-14 10:35:49 -0300

    desafio65 update

--- estrutura de controle/desafio65.py
+++ estrutura de controle/desafio65.py
@@ -1,11 +1,19 @@
 continuar = 'S'
-contador = acumulador = media = 0 
+contador = acumulador = media = maior = menor = 0 
 
 while continuar in 'Ss':
   numero = int(input('Digite um número: '))
   valor_continuar = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
   continuar = valor_continuar
-  acumulador += numero
+  acumulador += numero 
   contador += 1
+  if contador == 1:
+    maior = menor = numero
+  else:
+    if numero > maior:
+      maior = numero
+    if numero < menor:
+      menor = numero
 media = acumulador / contador
 print(f'Você digitou {contador} números e a média foi {media}')
+print(f'O maior valor foi {maior} e o menor foi {menor}')
