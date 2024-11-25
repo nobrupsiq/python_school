a254b5c0ee1b7ea79aedf7b3ffde649fef23ce25
bruno <bpires.siqueira@gmail.com>
2024-11-25 20:20:30 -0300

    desafio71

--- estrutura de controle/desafio71.py
+++ estrutura de controle/desafio71.py
@@ -3,6 +3,24 @@ Crie um programa que simule o funcionamento de um caixa eletrônico. No início,
 
 OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1
 """
+valor = int(input('Que valor você quer sacar? R$'))
+cedula = 50
+valor_de_saque = valor
+total_cedulas = 0
 
-valor_de_saque = int(input('Que valor você quer sacar? R$'))
-
+while True:
+  if valor_de_saque >= cedula: # SE O VALOR DO SAQUE FOR MAIOR QUE 50
+    valor_de_saque -= cedula # VAI DIMINUIR 50 DO VALOR QUE A PESSOA QUER SACAR
+    total_cedulas += 1 
+  else: # Agora se o valor do saque for menor que 50 ele cai nesse bloco else
+    if total_cedulas > 0:
+      print(f'Total de {total_cedulas} cédulas de R${cedula}')
+    if cedula == 50:
+      cedula = 20
+    elif cedula == 20:
+      cedula = 10
+    elif cedula == 10:
+      cedula = 1
+    total_cedulas = 0
+    if valor_de_saque == 0:
+      break
