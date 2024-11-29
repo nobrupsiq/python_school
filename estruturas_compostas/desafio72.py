a834257f0008db20c19dd792aefe52afc4269f57
bruno <bpires.siqueira@gmail.com>
2024-11-29 20:04:52 -0300

    desafio72 update

--- estruturas_compostas/desafio72.py
+++ estruturas_compostas/desafio72.py
@@ -2,8 +2,12 @@
 #Seu programa deverá ler um número pelo teclado(entre 0 20) e mostrá-lo por extenso.
 contagem = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco','Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezesete', 'Dezoito', 'Dezenove', 'Vinte')
 
-numero_do_usuario = int(input('Digite um número entre 0 e 20: '))
+while True:
+  numero_do_usuario = int(input('Digite um número entre 0 e 20: '))
+  while numero_do_usuario > 20 or numero_do_usuario < 0:
+    numero_do_usuario = int(input('Tente novamente. Digite um número entre 0 e 20: '))
 
-for index, contador in enumerate(contagem):
-  if index == numero_do_usuario:
-    print(contador)
+  for index, contador in enumerate(contagem):
+    if index == numero_do_usuario:
+      print(contador)
+  break
