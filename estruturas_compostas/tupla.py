5cd5832c21d527c0275149ea0ba3d0bfca4bcc40
bruno <bpires.siqueira@gmail.com>
2024-11-28 23:56:11 -0300

    tupla intro

---
+++ estruturas_compostas/tupla.py
@@ -0,0 +1,20 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+# Tuplas são imutáveis
+
+# for comida in lanche:
+#   print(f'Eu vou comer {comida}')
+
+# for cont in range(0, len(lanche)):
+#   print(lanche[cont], cont)
+
+# for pos, comida in enumerate(lanche):
+#   print(pos, comida)
+
+# a = (2, 5, 4)
+# b = (5, 8, 1, 2)
+# c = b + a
+# print(c)
+# print(c.index(4))
+
+pessoa = ('Bruno', 39, 'M', 99.98)
+print(pessoa)
