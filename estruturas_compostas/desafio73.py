d6b935c203adf50fb44c0750627c467d8dbdd19e
bruno <bpires.siqueira@gmail.com>
2024-12-03 11:26:37 -0300

    desafio73 update

--- estruturas_compostas/desafio73.py
+++ estruturas_compostas/desafio73.py
@@ -13,5 +13,5 @@ times = (
 )
 print(f'Lista de times do Brasileirão: {times}')
 print(f'Os 5 primeiros são: {times[0:5]}')
-print(f'Os 4 últimos são: {times[-1:-5:-1]}')
+print(f'Os 4 últimos são: {times[-4:]}')
 print(f'Times em ordem alfabética: {sorted(times)}')
