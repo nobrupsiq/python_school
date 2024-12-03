e7f0b51e3dd287ae487e061141e7c7f1a518667e
bruno <bpires.siqueira@gmail.com>
2024-12-03 13:39:39 -0300

    desafio74 update

--- estruturas_compostas/desafio74.py
+++ estruturas_compostas/desafio74.py
@@ -3,23 +3,35 @@ Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla
 
 Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla
 '''
-import random
 
-storage = ()
-menor_valor = 0
-maior_valor = 0
+ # MEU CODIGO
+from random import randint
 
-for i in range(1, 6):
-  numero_aleatorio = random.randint(1, 10)
-  storage += (numero_aleatorio,)
-  if i == 1:
-    menor_valor = storage[0]
-    maior_valor = storage[0]
-  for index, numero in enumerate(storage):
-    if numero < menor_valor:
-      menor_valor = numero
-    if numero > maior_valor:
-      maior_valor = numero
+# storage = ()
+# menor_valor = 0
+# maior_valor = 0
 
-print(storage)
-print(menor_valor, maior_valor)
+# for i in range(1, 6):
+#   numero_aleatorio = randint(1, 10)
+#   storage += (numero_aleatorio,)
+#   if i == 1:
+#     menor_valor = storage[0]
+#     maior_valor = storage[0]
+#   for index, numero in enumerate(storage):
+#     if numero < menor_valor:
+#       menor_valor = numero
+#     if numero > maior_valor:
+#       maior_valor = numero
+
+# print(storage)
+# print(f'O maior valor sorteado foi {maior_valor}')
+# print(f'O menor valor sorteado foi {menor_valor}')
+
+# CODIGO DO GUANABARA
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+
+print(f'Os valores sorteados foram: ', end='')
+for index in numeros:
+  print(f'{index} ', end='')
+print(f'\nO maior valor sorteado foi {max(numeros)}')
+print(f'O menor valor sorteado foi {min(numeros)}')
