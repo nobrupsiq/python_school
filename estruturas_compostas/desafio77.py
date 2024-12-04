6bfd8228f471564abb3d2bfd20ffdf3d7d39b0c4
bruno <bpires.siqueira@gmail.com>
2024-12-04 10:18:18 -0300

    desafio77

--- estruturas_compostas/desafio77.py
+++ estruturas_compostas/desafio77.py
@@ -2,3 +2,11 @@
 Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
 Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 """
+
+palavras = ('aprender', 'programar', 'linguagem', 'python', 'Curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programar', 'futuro')
+
+for index in palavras: 
+  print(f'\nNa palavra {index.upper()} temos as vogais ', end='')
+  for letra in index:
+    if letra.lower() in 'aeiou':
+      print(letra, end=' ')
